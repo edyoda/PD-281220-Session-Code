758b01a362acc61288dc202841ee7a8f2415ab30
Kallol Ghose <KG00360770@Techmahindra.com>
2021-06-29 18:46:56 +0530

    Updated PPT and static files

---
+++ session_static_files/monthly_challenge/challenges/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="all-challenges"),
+    path("<int:month>", views.monthly_challenge_number),
+    path("<str:month>", views.monthly_challenge, name="monthly-challenge"),
+]
